9b6952897ced450aa9e3d87860d3c390e4d5758a
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2025-12-26 15:42:22 -0300

    iniciando delegates

--- AdvancedCsharp/Program.cs
+++ AdvancedCsharp/Program.cs
@@ -4,6 +4,22 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CreatingDelegate();
+        WritingPlugInMethodsWithDelegates();
     }
+
+    private static void WritingPlugInMethodsWithDelegates()
+    {
+        
+    }
+
+    private static void CreatingDelegate()
+    {
+        MyTransformer t = Square; // shorthand to MyTransformer t = new MyTransformer(Square)
+        int answer = t(3); // t(3) is a shorthand to t.Invoke(3);
+        Console.WriteLine(answer);
+    }
+
+    private delegate int MyTransformer(int x);
+    private static int Square(int x) => x * x;
 }
