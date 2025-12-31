5eb0341c9f041b761fcc71c079f53f3a88374a47
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2025-12-31 11:13:54 -0300

    trabalhando com multicast delegates

---
+++ AdvancedCsharp/MyReporter.cs
@@ -0,0 +1,29 @@
+﻿namespace AdvancedCsharp;
+
+public delegate void ProgressReporter(int percentComplete);
+
+public class MyReporter
+{
+    public string Prefix { get; set; }
+
+    public void ReportProgress(int percentComplete) 
+        => Console.WriteLine($"{Prefix} {percentComplete}%");
+}
+
+public class Util
+{
+    public static void HardWork(ProgressReporter multicastDelegate)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            multicastDelegate(i * 10);
+            Thread.Sleep(100);
+        }
+    }
+    
+    public static void WriteProgressToConsole(int percentComplete)
+        => Console.WriteLine(percentComplete);
+    
+    public static void WriteProgressToFile(int percentComplete) 
+        => System.IO.File.AppendAllText("progress.txt", $"%Completed: {percentComplete}%\n");
+}
--- AdvancedCsharp/Program.cs
+++ AdvancedCsharp/Program.cs
@@ -1,16 +1,69 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using AdvancedCsharp;
+
 internal class Program
 {
     public static void Main(string[] args)
     {
-        CreatingDelegate();
-        WritingPlugInMethodsWithDelegates();
+        // CreatingDelegate();
+        // WritingPlugInMethodsWithDelegates();
+        // InstanceTarget();
+        MulticastDelegate();
+    }
+
+    private static void MulticastDelegate()
+    {
+        /*
+         * SEE PAGES 269-271 TO CODE SAMPLES
+         * 
+         * ALL DELEGATE INSTANCES HAVE 'MULTICAST CAPABILITY'. THIS MEANS THAT A DELEGATE
+         * INSTANCE CAN REFERENCE NOT JUST A SINGLE TARGET METHOD BUT ALSO A LIST OF TAR-
+         * GET METHODS. THE +, - AND += OR -= OPERATORS COMBINE OR REMOVE DELEGATE INSTANCES.
+         *
+         * DELEGATES ARE IMMUTABLE, SO WHEN YOU CALL += OR -=, YOU'RE IN FACT CREATING A NEW
+         * DELEGATE INSTANCE AND ASSIGNING IT TO THE EXISTING VARIABLE.
+         *
+         * IF A MULTICAST DELEGATE HAS A NONVOID RETURN TYPE, THE CALLER RECEIVES THE RETURN
+         * VALUE FROM THE LAST METHOD TO BE INVOKED. THE PRECEDING METHODS ARE STILL CALED,
+         * BUT THEIR RETURN VALUES ARE DISCARDED. FOR MOST SCENARIOS IN WHICH MULTICAST DELE-
+         * GATES ARE USED, THEY HAVE VOID RETURN TYES, SO THIS SUBTLETY(PECULIARIDADE) DOES
+         * NOT ARISE.
+         */
+
+        ProgressReporter multicastDelegate = Util.WriteProgressToConsole;
+        multicastDelegate += Util.WriteProgressToFile;
+        Console.WriteLine(multicastDelegate.Target is null);
+        
+        if (System.IO.File.Exists("progress.txt"))
+        {
+            System.IO.File.Delete("progress.txt");
+        }
+        Util.HardWork(multicastDelegate);
+
+    }
+    
+    /*  When an instance method is assigned to a delegate obect, the latter
+        maintains a reference not only to the method but also to the instance to
+        which the method belongs.
+     */
+    private static void InstanceTarget()
+    {
+        MyReporter r = new MyReporter();
+        r.Prefix = "%Completed: ";
+        ProgressReporter p = r.ReportProgress;
+        p(99);
+        Console.WriteLine(p.Target == r);
+        Console.WriteLine(p.Method);
+
+        r.Prefix = "";
+        p(100);
+
     }
 
     private static void WritingPlugInMethodsWithDelegates()
     {
-        int[] values = new [] {  1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int[] values = new [] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         
         Transformer(values, Square);
 
