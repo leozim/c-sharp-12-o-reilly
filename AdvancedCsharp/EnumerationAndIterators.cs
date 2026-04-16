26de2afd8238254102914c4774e46e5527aeb11d
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-04-16 15:39:45 -0300

    studying enumeration and iteraor

---
+++ AdvancedCsharp/EnumerationAndIterators.cs
@@ -0,0 +1,60 @@
+﻿namespace AdvancedCsharp;
+
+public class LearningEnumeration
+{
+    /*
+     * Here is the high-level way of iterating through the characters in the word "bee"
+     * using a FOREACH statement
+     */
+    public static void HighLevelIterator()
+    {
+        foreach (char c in "beer") 
+            Console.WriteLine(c);
+    }
+
+    public static void LowLevelIterator()
+    {
+        using (var enumerator = "beer".GetEnumerator())
+        {
+            while (enumerator.MoveNext())
+            {
+                var element = enumerator.Current;
+                Console.WriteLine(element);
+            }
+            
+        }
+    }
+}
+
+public class LearningIterators
+{
+    /*
+     * Whereas a FOREACH statemente is a consumer of a enumerator, an iterator is
+     * a produces of an enumerator.
+     * Below we'll using an iterator to return a fib's sequence
+     */
+
+    public IEnumerable<int> Fibs(int fibCount)
+    {
+        for (int i = 0, prevFib = 1, curFib = 1; fibCount < i++)
+        {
+            yield return prevFib;
+            int newFib = prevFib + curFib;
+            prevFib = curFib;
+            curFib = newFib;
+        }
+    }
+
+    public IEnumerable<string> Foo()
+    {
+        yield return "foo";
+        yield return "bar";
+        yield return "baz";
+    }
+    
+    /*
+     * A 'return' statement is illegal in an iterator block; instead you must use the
+     * 'yield break' statement to indicate that the iterator block should exit early,
+     * without returning more element.
+     */
+}
