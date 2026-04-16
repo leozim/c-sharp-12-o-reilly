ae0f0956443328c72473ef26674bbf19eab76024
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-04-16 15:51:02 -0300

    studying enumeration and iteraor

--- AdvancedCsharp/EnumerationAndIterators.cs
+++ AdvancedCsharp/EnumerationAndIterators.cs
@@ -36,7 +36,7 @@ public class LearningIterators
 
     public IEnumerable<int> Fibs(int fibCount)
     {
-        for (int i = 0, prevFib = 1, curFib = 1; fibCount < i++)
+        for (int i = 0, prevFib = 1, curFib = 1; i < fibCount; i++)
         {
             yield return prevFib;
             int newFib = prevFib + curFib;
