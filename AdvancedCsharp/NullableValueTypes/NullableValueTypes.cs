829772314873d0b4450c88569a7f554baac09c6b
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-05-21 01:53:56 -0300

    nullable

---
+++ AdvancedCsharp/NullableValueTypes/NullableValueTypes.cs
@@ -0,0 +1,20 @@
+﻿namespace AdvancedCsharp.NullableValueTypes;
+
+public static class NullableValueTypes
+{
+    // Instead of reference tupes Value type cannot represent null values
+    public static void S()
+    {
+        string s = null; // reference type
+        // int i = null; // compile error
+        int? ii = null; // we must use ? to value type accept null
+        
+        /*
+         * The conversion from T to T? is implicit, whereas from T? to T the
+         * conversion is explicti
+         */
+        int? x = 5; // implicit
+        int y = (int) x; // explicit
+    }
+
+}
